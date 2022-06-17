0cb214977a3196df1fd99513827f81019a36b5d1
fares <fhalasahhh@gmail.com>
2022-06-17 18:44:43 +0300

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Refine } from "@pankod/refine-core";
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-graphql";
+import { GraphQLClient } from "graphql-request";
+
+const API_URL = "https://your-graphql-url/graphql";
+const client = new GraphQLClient(API_URL);
+const gqlDataProvider = dataProvider(client);
+import { authProvider } from "./authProvider";
+
+function App() {
+  return (
+    <Refine
+      routerProvider={routerProvider}
+      dataProvider={gqlDataProvider}
+      authProvider={authProvider}
+    />
+  );
+}
+
+export default App;
