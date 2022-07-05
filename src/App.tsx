ca0c6885f981c21ff97c59161ba13396522b0aec
X-Anas-X <anas.zain1996@gmail.com>
2022-07-05 20:52:12 +0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import { Refine } from "@pankod/refine-core";
 import routerProvider from "@pankod/refine-react-router-v6";
-import dataProvider from "@pankod/refine-graphql";
 import { GraphQLClient } from "graphql-request";
 import { authProvider } from "./authProvider";
 import {
-  List,
-  TagField,
-  DateField,
-  Table,
-  useTable,
   Layout,
 } from "@pankod/refine-antd";
 import "@pankod/refine-antd/dist/styles.min.css";
 import LoginPage from "Containers/LoginPage";
-import UsersList from "Containers/UsersList";
 import Dashboard from "Containers/Dashboard";
 import "./App.css";
 import Logo from "./Assets/Images/Rectangle.png";
+import { UsersList } from "Containers/UsersList/UsersList";
+import dataProvider from "Containers/DataProvider";
+import ShowUsers from "Containers/UsersList/ShowUsers";
+import './index.css';
+import { EditUser } from "Containers/UsersList/EditUser";
+import { FiUsers } from 'react-icons/fi'
+import { OrderesList } from "Containers/Orders/OrdersListing";
+import ShowOrder from "Containers/Orders/ShowOrder";
+import { BiReceipt } from 'react-icons/bi';
 
 const API_URL = "https://clikstaging.herokuapp.com/graphql";
 const client = new GraphQLClient(API_URL);
@@ -50,15 +52,21 @@ function App() {
           // icon: <BsBuilding size={20} />,
         },
         {
-          name: "Users",
+          name: "findUsers",
           options: { label: "Users" },
           list: UsersList,
-          // icon: <BsBuilding size={20} />,
+          show: ShowUsers,
+          edit: EditUser,
+          icon: <FiUsers size={20} />,
         },
-        // {
-        //   name: "users",
-        //   list: UsersList,
-        // },
+        {
+          name: "findAllOrders",
+          options: { label: "Orders" },
+          list: OrderesList,
+          show: ShowOrder,
+          edit: EditUser,
+          icon: <BiReceipt size={20} />,
+        },
       ]}
       Layout={Layout}
       LoginPage={LoginPage}
