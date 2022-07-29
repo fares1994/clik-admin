8ef570bf24b85fb27964b479892c744ac2bc6a07
fares <fhalasahhh@gmail.com>
2022-07-29 16:08:08 +0300

    edit title done

--- src/Containers/UsersList/EditUser.tsx
+++ src/Containers/UsersList/EditUser.tsx
@@ -1,7 +1,6 @@
 import { IResourceComponentsProps } from "@pankod/refine-core";
 import React, { useEffect, useState } from "react";
 import {
-  useForm,
   Form,
   Input,
   Edit,
@@ -9,21 +8,21 @@ import {
   ListButton,
   Typography,
   ShowButton,
-  Spin,
 } from "@pankod/refine-antd";
-import { account } from "Containers/QueryReturns";
+import { account, titles } from "Containers/QueryReturns";
 import {
   UpdateRecordAction,
   showRecord,
+  GetListAction,
 } from "Containers/Actions/ConfigsActions";
 import { useFormik } from "formik";
 import { useParams } from "react-router-dom";
 const { Text } = Typography;
 
 interface Inputs {
-  updateUserInput: {
+  updateUserByIdInput: {
     value: {
-      id?: string;
+      userId?: string;
       name?: string;
       email?: string;
       personalBio?: string;
@@ -41,59 +40,68 @@ export const EditUser: React.FC<IResourceComponentsProps> = () => {
   const params = useParams();
   const [record, setRecord] = useState<any>();
   const [refresh, setRefresh] = useState<boolean>(true);
-
+  const [titlesList, setTitlesList] = useState<any>();
   useEffect(() => {
     if (refresh && params?.id) {
       showRecord("findUserAuth", params?.id, account, setRecord, setRefresh);
     }
   }, [refresh, params?.id]);
+
+  useEffect(() => {
+    if (refresh && params?.id) {
+      GetListAction("findTitles", undefined, titles, setTitlesList);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   const handleRefetch = () => setRefresh(true);
 
   const { values, handleChange, setFieldValue, handleSubmit } = useFormik({
     initialValues: {
-      id: record?.id,
+      userId: record?.id,
       name: "",
       email: "",
       personalBio: "",
-      gender: "",
+      // gender: "",
       title: "",
       instagramId: "",
       twitterId: "",
     },
     onSubmit: async (submittedValues) => {
       const inputs: Inputs = {
-        updateUserInput: {
+        updateUserByIdInput: {
           value: {
-            // id: record?.id
+            userId: record?.id,
           },
           required: true,
           type: "UpdateUserInput",
         },
       };
       if (submittedValues.name) {
-        inputs.updateUserInput.value.name = submittedValues.name;
+        inputs.updateUserByIdInput.value.name = submittedValues.name;
       }
       //   if (submittedValues.email) {
       //     inputs.updateUserInput.value.email = submittedValues.email;
       //   }
-      if (submittedValues.gender) {
-        inputs.updateUserInput.value.gender = submittedValues.gender;
-      }
+      // if (submittedValues.gender) {
+      //   inputs.updateUserByIdInput.value.gender = submittedValues.gender;
+      // }
       if (submittedValues.personalBio) {
-        inputs.updateUserInput.value.personalBio = submittedValues.personalBio;
+        inputs.updateUserByIdInput.value.personalBio =
+          submittedValues.personalBio;
       }
 
       if (submittedValues.instagramId) {
-        inputs.updateUserInput.value.instagramId = submittedValues.instagramId;
+        inputs.updateUserByIdInput.value.instagramId =
+          submittedValues.instagramId;
       }
       if (submittedValues.title) {
-        inputs.updateUserInput.value.title = submittedValues.title;
+        inputs.updateUserByIdInput.value.title = submittedValues.title;
       }
       if (submittedValues.twitterId) {
-        inputs.updateUserInput.value.twitterId = submittedValues.twitterId;
+        inputs.updateUserByIdInput.value.twitterId = submittedValues.twitterId;
       }
-
-      UpdateRecordAction("updateUser", inputs, handleRefetch);
+      console.log(inputs, "inputsi");
+      UpdateRecordAction("updateUserById", inputs, handleRefetch);
     },
   });
 
@@ -108,12 +116,10 @@ export const EditUser: React.FC<IResourceComponentsProps> = () => {
       setFieldValue("name", record?.name);
       setFieldValue("personalBio", record?.personalBio);
       setFieldValue("email", record?.email);
-      setFieldValue("title", record?.title?.title);
-      setFieldValue("gender", record?.gender);
+      setFieldValue("title", record?.title?._id);
+      // setFieldValue("gender", record?.gender);
     }
   }, [record, setFieldValue]);
-
-  if (!values?.gender) return <Spin className="spinner" size={"large"} />;
 
   return (
     <Edit
@@ -136,11 +142,18 @@ export const EditUser: React.FC<IResourceComponentsProps> = () => {
         <Form.Item label="Name" rules={[{ required: true }]} required>
           <Input name="name" onChange={handleChange} value={values.name} />
         </Form.Item>
-        {/* <Form.Item label="Email" rules={[{ required: true }]} required>
-          <Input name="email" onChange={handleChange} value={values.email} />
-        </Form.Item> */}
-        <Form.Item label="Title" required rules={[{ required: true }]}>
-          <Input name="title" onChange={handleChange} value={values.title} />
+        <Form.Item label="Title">
+          <Select
+            value={values?.title || ""}
+            options={
+              titlesList?.map((title: any) => ({
+                label: title.title,
+                value: title._id,
+              })) || []
+            }
+            onChange={(value) => setFieldValue("title", value)}
+            defaultValue={values?.title || ""}
+          />
         </Form.Item>
         <Form.Item label="Personal Bio" rules={[{ required: true }]}>
           <Input.TextArea
@@ -149,7 +162,7 @@ export const EditUser: React.FC<IResourceComponentsProps> = () => {
             value={values.personalBio}
           />
         </Form.Item>
-        <Form.Item label="Gender" required>
+        {/* <Form.Item label="Gender" required>
           <Select
             defaultValue={record?.gender}
             options={[
@@ -158,7 +171,7 @@ export const EditUser: React.FC<IResourceComponentsProps> = () => {
             ]}
             onChange={(val) => setFieldValue("gender", val)}
           />
-        </Form.Item>
+        </Form.Item> */}
 
         <Form.Item label="TwitterId" required>
           <Input
